76673d4d68e8eab366a404019fdb2bc6dc7e4cdd
ohr486 <ohr486@gmail.com>
2017-09-13 17:49:00 +0900

    remove useless files

--- tmp/sample/sample.py
+++
@@ -1,8 +0,0 @@
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def lambda_handler(event, context):
-    logger.info('got event{}'.format(event))
-    logger.error('something went wrong!')
-    return 'Hello, first lambda func!'
